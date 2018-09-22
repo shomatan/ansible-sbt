261f215ca58aed9e173e11c069ad1996ecdf1981
Shoma Nishitateno <shoma416@gmail.com>
2018-09-22 23:20:18 +0900

    add: laravel 5.5 - php 7.2.10

---
+++ 5.5-php-7.2/docker-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# -----------------------------------------------------------------------------
+set +e
+
+echo "** Preparing Laravel app"
+
+cp -r /home/www/app/* .
+
+cat > .env <<EOF
+APP_ENV=${LARAVEL_APP_ENV}
+APP_DEBUG=${LARAVEL_APP_DEBUG}
+APP_KEY=
+ 
+DB_HOST=${LARAVEL_DB_HOST}
+DB_DATABASE=${LARAVEL_DB_DATABASE}
+DB_USERNAME=${LARAVEL_DB_USERNAME}
+DB_PASSWORD=${LARAVEL_DB_PASSWORD}
+ 
+CACHE_DRIVER=file
+SESSION_DRIVER=file
+QUEUE_DRIVER=sync
+ 
+MAIL_DRIVER=smtp
+MAIL_HOST=mailtrap.io
+MAIL_PORT=2525
+MAIL_USERNAME=null
+MAIL_PASSWORD=null
+MAIL_ENCRYPTION=null
+EOF
+
+php artisan key:generate
+ 
+echo "** Waiting for MySQL"
+until php artisan migrate --force; do
+  >&2 echo "**** MySQL is unavailable - sleeping"
+  sleep 1
+done
+ 
+echo "** Database seeding"
+php artisan db:seed --force
+ 
+chown -R www:www $LARAVEL_HOME
+
+echo "########################################################"
+ 
+echo "** Executing php-fpm"
+ 
+exec php-fpm -F
